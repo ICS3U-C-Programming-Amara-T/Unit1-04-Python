edc71dcb888587c4d5a4a3879552d293a376a20f
Amara Tie <amara.tie@stu.ocsb.ca>
2025-02-18 10:03:38 +0000

    How to do math on Python

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Amara Tie 
+
+# date: 14: Feburary 2025
+
+# This program shows how to do basic math in python
+
+def main():
+    print(2+3)
+    print(5-4)
+    print(8/3)
+    print(3**2)
+    print(4**0.5)
+if __name__ == '__main__':
+    main()
